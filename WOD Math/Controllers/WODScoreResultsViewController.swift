c49def4d5789e6c5c872c373206b6ffdac3670c3
Charlie Eison <charlie@esnenterprises.com>
2020-04-09 16:26:03 -0400

    Finished second VC and removed particles

--- WOD Math/Controllers/WODScoreResultsViewController.swift
+++ WOD Math/Controllers/WODScoreResultsViewController.swift
@@ -18,24 +18,24 @@ class WODScoreResultsViewController: UIViewController {
         additionalRepsCompletedLabel.text = "\(additionalRepsCompleted)"
         finalScoreLabel.text = "\(Int(finalScore))"
         
-        let skView = SKView(frame: view.frame)
-        view.addSubview(skView)
-        
-        let scene = SKScene(size: view.bounds.size)
-        scene.anchorPoint = CGPoint(x: 0.5, y: 1)
-        
-        if let particles = SKEmitterNode(fileNamed: "Confetti") {
-            scene.addChild(particles)
-            scene.backgroundColor = .clear
-            skView.allowsTransparency = true
-            skView.backgroundColor = .clear
-            
-            skView.presentScene(scene)
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                particles.particleBirthRate = 0
-            }
-        }
+//        let skView = SKView(frame: view.frame)
+//        view.addSubview(skView)
+//        
+//        let scene = SKScene(size: view.bounds.size)
+//        scene.anchorPoint = CGPoint(x: 0.5, y: 1)
+//        
+//        if let particles = SKEmitterNode(fileNamed: "Confetti") {
+//            scene.addChild(particles)
+//            scene.backgroundColor = .clear
+//            skView.allowsTransparency = true
+//            skView.backgroundColor = .clear
+//            
+//            skView.presentScene(scene)
+//            
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//                particles.particleBirthRate = 0
+//            }
+//        }
     }
 }
 
