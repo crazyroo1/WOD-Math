c27c0c30ffc6e674e33276cdabdaf8b5d3770cd7
Charlie Eison <charlie@esnenterprises.com>
2020-04-09 16:13:43 -0400

    added constraints for WODDetailsView

--- WOD Math/Controllers/WODScoreResultsViewController.swift
+++ WOD Math/Controllers/WODScoreResultsViewController.swift
@@ -1,5 +1,5 @@
-
 import UIKit
+import SpriteKit
 
 class WODScoreResultsViewController: UIViewController {
     
@@ -11,13 +11,33 @@ class WODScoreResultsViewController: UIViewController {
     var fullRoundsCompleted = 0
     var additionalRepsCompleted = 0
     var finalScore = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fullRoundsCompletedLabel.text = ("\(fullRoundsCompleted)")
         additionalRepsCompletedLabel.text = "\(additionalRepsCompleted)"
         finalScoreLabel.text = "\(Int(finalScore))"
+        
+        let skView = SKView(frame: view.frame)
+        view.addSubview(skView)
+        
+        let scene = SKScene(size: view.bounds.size)
+        scene.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        if let particles = SKEmitterNode(fileNamed: "Confetti") {
+            scene.addChild(particles)
+            scene.backgroundColor = .clear
+            skView.allowsTransparency = true
+            skView.backgroundColor = .clear
+            
+            skView.presentScene(scene)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                particles.particleBirthRate = 0
+            }
+        }
     }
 }
+
 
 
